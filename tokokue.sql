8c6957d4fa4f3703f2471d14b0efc922d1ef6f5a
Yuma Arganto <84946624+yumaarganto@users.noreply.github.com>
2024-08-14 14:39:10 +0700

    Add files via upload

---
+++ tokokue.sql
@@ -0,0 +1,355 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 06, 2024 at 10:32 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tokokue`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(255) DEFAULT NULL,
+  `id_kategori` int(11) DEFAULT NULL,
+  `harga` int(11) DEFAULT NULL,
+  `deskripsi` mediumtext DEFAULT NULL,
+  `gambar` text DEFAULT NULL,
+  `berat` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`id_barang`, `nama_barang`, `id_kategori`, `harga`, `deskripsi`, `gambar`, `berat`) VALUES
+(8, 'Kue Ultah Nigga Capucino Sanhok', 11, 200000, 'rasanya seperti mati lampu...', 'kue_ultah_wafer.jpg', 1000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_gambar`
+--
+
+CREATE TABLE `tbl_gambar` (
+  `id_gambar` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `ket` varchar(255) DEFAULT NULL,
+  `gambar` text DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_gambar`
+--
+
+INSERT INTO `tbl_gambar` (`id_gambar`, `id_barang`, `ket`, `gambar`) VALUES
+(27, 8, 'Uniforms', 'logouniforms.jpeg'),
+(26, 8, 'Mantap nggih', 'untadlogo.png'),
+(29, 9, 'pak luhut', 'opung.jpeg'),
+(30, 9, 'ertertrtrt', 'software_kaos.PNG');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kategori`
+--
+
+CREATE TABLE `tbl_kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(50) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_kategori`
+--
+
+INSERT INTO `tbl_kategori` (`id_kategori`, `nama_kategori`) VALUES
+(10, 'Opera Cake'),
+(11, 'Blackforest');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_pelanggan`
+--
+
+CREATE TABLE `tbl_pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `nama_pelanggan` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `foto` text DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_pelanggan`
+--
+
+INSERT INTO `tbl_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `email`, `password`, `foto`) VALUES
+(7, 'agus', 'agus@gmail.com', 'agus', NULL),
+(8, 'Yuma', 'yumaarganto1@gmail.com', '123', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_rekening`
+--
+
+CREATE TABLE `tbl_rekening` (
+  `id_rekening` int(11) NOT NULL,
+  `nama_bank` varchar(25) DEFAULT NULL,
+  `no_rek` varchar(25) DEFAULT NULL,
+  `atas_nama` varchar(25) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_rekening`
+--
+
+INSERT INTO `tbl_rekening` (`id_rekening`, `nama_bank`, `no_rek`, `atas_nama`) VALUES
+(1, 'BRI', '5434-4382-3434-4345', 'MOH. AGUNG NURSALIM');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_rinci_transaksi`
+--
+
+CREATE TABLE `tbl_rinci_transaksi` (
+  `id_rinci` int(11) NOT NULL,
+  `no_order` varchar(25) DEFAULT NULL,
+  `id_barang` int(11) DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_rinci_transaksi`
+--
+
+INSERT INTO `tbl_rinci_transaksi` (`id_rinci`, `no_order`, `id_barang`, `qty`) VALUES
+(26, '20220623MYH0TZYA', 8, 2),
+(27, '20220623VBQA8QGL', 8, 1),
+(28, '20220623EZNAYZAQ', 8, 1),
+(29, '202206235CDEUAC0', 8, 1),
+(30, '20220624M6RYOZUY', 9, 2),
+(31, '20220624M6RYOZUY', 8, 1),
+(32, '20240805Y3SU85H6', 8, 1),
+(33, '20240806WULD3WYO', 8, 1),
+(34, '20240806GACZU3I6', 8, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_setting`
+--
+
+CREATE TABLE `tbl_setting` (
+  `id` int(1) NOT NULL,
+  `nama_toko` varchar(255) DEFAULT NULL,
+  `lokasi` int(11) DEFAULT NULL,
+  `alamat_toko` text DEFAULT NULL,
+  `no_telpon` varchar(15) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_setting`
+--
+
+INSERT INTO `tbl_setting` (`id`, `nama_toko`, `lokasi`, `alamat_toko`, `no_telpon`) VALUES
+(1, 'Pekanbaru Creative', 350, 'Jl. Pasir Putih Jl. Raya Pandau Permai No.simpang, Pandau Jaya, Kec. Siak Hulu, Kabupaten Kampar, Riau 28284', '0885271540924');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_transaksi`
+--
+
+CREATE TABLE `tbl_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_pelanggan` int(11) DEFAULT NULL,
+  `no_order` varchar(25) NOT NULL,
+  `tgl_order` date DEFAULT NULL,
+  `nama_penerima` varchar(25) DEFAULT NULL,
+  `hp_penerima` varchar(15) DEFAULT NULL,
+  `provinsi` varchar(25) DEFAULT NULL,
+  `kota` varchar(25) DEFAULT NULL,
+  `alamat` text DEFAULT NULL,
+  `kode_pos` varchar(8) DEFAULT NULL,
+  `expedisi` varchar(255) DEFAULT NULL,
+  `paket` varchar(255) DEFAULT NULL,
+  `estimasi` varchar(255) DEFAULT NULL,
+  `ongkir` int(11) DEFAULT NULL,
+  `berat` int(11) DEFAULT NULL,
+  `grand_total` int(11) DEFAULT NULL,
+  `total_bayar` int(11) DEFAULT NULL,
+  `status_bayar` int(1) DEFAULT NULL,
+  `bukti_bayar` text DEFAULT NULL,
+  `atas_nama` varchar(25) DEFAULT NULL,
+  `nama_bank` varchar(25) DEFAULT NULL,
+  `no_rek` varchar(25) DEFAULT NULL,
+  `status_order` int(1) DEFAULT NULL,
+  `no_resi` varchar(25) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_transaksi`
+--
+
+INSERT INTO `tbl_transaksi` (`id_transaksi`, `id_pelanggan`, `no_order`, `tgl_order`, `nama_penerima`, `hp_penerima`, `provinsi`, `kota`, `alamat`, `kode_pos`, `expedisi`, `paket`, `estimasi`, `ongkir`, `berat`, `grand_total`, `total_bayar`, `status_bayar`, `bukti_bayar`, `atas_nama`, `nama_bank`, `no_rek`, `status_order`, `no_resi`) VALUES
+(27, 8, '20240806GACZU3I6', '2024-08-06', 'Agus', '2312321312', 'Riau', '350', 'Jl mahang raya blok c 50 no 19', '28452', 'tiki', 'T15', '4 Hari', 191000, 1000, 200000, 391000, 1, 'phourto.jpg', 'fsfasf', 'fdsfds', 'dasdas', 3, '12345');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(25) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci ROW_FORMAT=DYNAMIC;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id_user`, `nama_user`, `username`, `password`) VALUES
+(1, 'Superadmin', 'superadmin', '12345'),
+(2, 'Admin ', 'admin', '12345'),
+(7, 'Owner', 'owner', '12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`id_barang`) USING BTREE;
+
+--
+-- Indexes for table `tbl_gambar`
+--
+ALTER TABLE `tbl_gambar`
+  ADD PRIMARY KEY (`id_gambar`) USING BTREE;
+
+--
+-- Indexes for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  ADD PRIMARY KEY (`id_kategori`) USING BTREE;
+
+--
+-- Indexes for table `tbl_pelanggan`
+--
+ALTER TABLE `tbl_pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`) USING BTREE;
+
+--
+-- Indexes for table `tbl_rekening`
+--
+ALTER TABLE `tbl_rekening`
+  ADD PRIMARY KEY (`id_rekening`) USING BTREE;
+
+--
+-- Indexes for table `tbl_rinci_transaksi`
+--
+ALTER TABLE `tbl_rinci_transaksi`
+  ADD PRIMARY KEY (`id_rinci`) USING BTREE;
+
+--
+-- Indexes for table `tbl_setting`
+--
+ALTER TABLE `tbl_setting`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- Indexes for table `tbl_transaksi`
+--
+ALTER TABLE `tbl_transaksi`
+  ADD PRIMARY KEY (`id_transaksi`) USING BTREE;
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id_user`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tbl_gambar`
+--
+ALTER TABLE `tbl_gambar`
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `tbl_kategori`
+--
+ALTER TABLE `tbl_kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `tbl_pelanggan`
+--
+ALTER TABLE `tbl_pelanggan`
+  MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `tbl_rekening`
+--
+ALTER TABLE `tbl_rekening`
+  MODIFY `id_rekening` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tbl_rinci_transaksi`
+--
+ALTER TABLE `tbl_rinci_transaksi`
+  MODIFY `id_rinci` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT for table `tbl_transaksi`
+--
+ALTER TABLE `tbl_transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
